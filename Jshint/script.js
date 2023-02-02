0a9a10c7b92eb01ca5da0cbdbccdb2ec153cd924
PattaevAsadbek <bekasad088@gmail.com>
2023-02-02 23:04:36 +0900

    first commit

---
+++ Jshint/script.js
@@ -0,0 +1,29 @@
+// console.log("hello");
+// const ClientName = "asad";
+// console.log(ClientName);
+// let number = 12;
+// console.log(Number);
+// const isVictory = true;
+// console.log(isVictory);
+// const thief = {
+//   age: 18,
+//   hisname: "qosim",
+// };
+// console.log(thief);
+
+// const myFavoruiteColor = [];
+// myFavoruiteColor[0] = prompt("what is your favourite color #1");
+// myFavoruiteColor[1] = prompt("what is your favourite color #2");
+// myFavoruiteColor[2] = prompt("what is your favourite color #3");
+// console.log(myFavoruiteColor);
+const money = prompt("My money dont jiggle jigle");
+
+console.log(`https://youtube.com/samarbadriddinov ${money}`);
+
+let incr = 10;
+let decr = 10;
+console.log(++incr, --decr);
+
+console.log(10 % 3);
+
+console.log(5 * 5 === "25");
